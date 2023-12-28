31d84288484faad2aba0692bbff8309ac938edb0
Sonu Mondal <sonu.mondal4598@gmail.com>
2023-12-28 10:00:06 +0530

    added java 21 virtual threading properties in pom.xml file

---
+++ blog_application/src/main/java/com/blog/application/utils/SecurityConfig.java
@@ -0,0 +1,14 @@
+package com.blog.application.utils;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	
+
+}
