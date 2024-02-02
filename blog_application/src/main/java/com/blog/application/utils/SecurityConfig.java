604d624a3d6ae8daeca7d06d99701f92e53fddf5
Sonu Mondal <sonu.mondal4598@gmail.com>
2024-02-02 22:59:11 +0530

    modifying the class

--- blog_application/src/main/java/com/blog/application/utils/SecurityConfig.java
+++ blog_application/src/main/java/com/blog/application/utils/SecurityConfig.java
@@ -1,12 +1,16 @@
 package com.blog.application.utils;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
 
 @Configuration
 @EnableWebSecurity
+@EnableWebMvc
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 	
 	
