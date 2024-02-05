6e94e0294a5d9e1d656d5baca2bed11c08e177ba
Sonu Mondal <sonu.mondal4598@gmail.com>
2024-02-05 23:30:24 +0530

    adding custom method of user entity

--- blog_application/src/main/java/com/blog/application/repository/UserRepository.java
+++ blog_application/src/main/java/com/blog/application/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.blog.application.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.blog.application.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
 
-	
+	Optional<User> findByEmail(String email);
 }
