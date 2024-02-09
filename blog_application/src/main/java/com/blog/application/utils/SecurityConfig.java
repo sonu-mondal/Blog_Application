a0c39bc89b50053f14e3f588cbab0f4f79aa6e9e
Sonu Mondal <sonu.mondal4598@gmail.com>
2024-02-09 10:40:50 +0530

    .

--- blog_application/src/main/java/com/blog/application/utils/SecurityConfig.java
+++ blog_application/src/main/java/com/blog/application/utils/SecurityConfig.java
@@ -1,18 +1,47 @@
 package com.blog.application.utils;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import com.blog.application.Security.JWTAuthenticationEntryPoint;
+import com.blog.application.Security.JwtAuthenticatorFilter;
 
 
+
+//@SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
 @EnableWebMvc
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+//	
+//	@Autowired
+//	private CustomUs
 	
+	@Autowired
+	private JWTAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+	
+	@Autowired
+	private JwtAuthenticatorFilter jwtAuthenticatorFilter;
+	
+	@SuppressWarnings("removal")
+	@Bean
+	public SecurityFilterChain securityFilterChain( HttpSecurity http) {
+		http.
+		csrf().disable()
+		.authorizeHttpRequests()
+		.anyRequest()
+		.authenticated()
+		.and()
+		.httpBasic();
+		
+	}
 	
 
 }
